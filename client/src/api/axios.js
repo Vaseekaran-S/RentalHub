028ebf4109e51c4fb800867d782bff079a15b9d7
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-10-18 21:30:54 +0530

    test: axios instance issue

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -6,21 +6,10 @@ const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 let instance = axios.create({
     baseURL,
     headers: {
-        // This is a custom header, but CORS needs to be allowed on the server for it
-        'x-user-timezone': timeZone,
-        // Add Content-Type if you're sending JSON data
-        'Content-Type': 'application/json',
+        'x-user-timezone': timeZone,  // Custom header for timezone
+        'Content-Type': 'application/json',  // Default content type
+        'Authorization': 'Bearer token_here',  // Add authorization token if needed
     }
-});
-
-// Default headers can be set globally for all requests
-instance.defaults.headers.common['x-user-timezone'] = timeZone;
-
-// Optionally, handle preflight for cross-origin requests
-instance.interceptors.request.use((config) => {
-    config.headers['Access-Control-Allow-Origin'] = '*'; // Set to '*' or specific origin
-    config.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, x-user-timezone';
-    return config;
 });
 
 export default instance;
