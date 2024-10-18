3a5fe786b2f64d192a2337413d1182f03efcab46
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-10-18 21:39:07 +0530

    test: client axios instance config

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -9,7 +9,8 @@ let instance = axios.create({
         'x-user-timezone': timeZone,  // Custom header for timezone
         'Content-Type': 'application/json',  // Default content type
         'Authorization': 'Bearer token_here',  // Add authorization token if needed
-    }
+    },
+    withCredentials: false // Ensure this is false unless you're sending cookies to another domain
 });
 
 export default instance;
