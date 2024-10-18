d0beb5ab841093fa4e5afb37bef34651d1e16d24
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-10-18 20:20:14 +0530

    test: fix cors issue

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose")
 
+app.use(cors());
+
+app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    });
+    next();
+});
 
 app.get("/", (req, res) => {
     res.send("Hi");
